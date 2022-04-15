e86df415c2058e430590146566b90a1da2bc1510
manujtrehan <trehan.manuj@gmail.com>
2022-04-14 23:28:41 -0400

    Bug fixes: - Fixed YAML path using rospkg - Geodetic2enu returns np.float64, converted that to python float to set as a rosparam - Shifted gps2local() function call to the constructor - added yaml file in config folder to read gt gps location of fires

--- firefly_mapping/src/gps_to_local.py
+++ firefly_mapping/src/gps_to_local.py
@@ -3,28 +3,31 @@
 import rospy
 import pymap3d as pm
 import yaml
+import rospkg
 
 class GPS2LocalENU:
     def __init__(self):
-        with open('../config/gps_hotspots.yaml', 'r') as f:
+        rospack = rospkg.RosPack()
+        with open(rospack.get_path("firefly_mapping") + "/config/gps_hotspots.yaml", 'r') as f:
             self.d = yaml.load(f, Loader=yaml.FullLoader)
         
         self.lat0, self.lon0, self.h0 = rospy.get_param("local_pos_ref")
+
+        self.gps2local()
 
     def gps2local(self):
         all_keys = list(self.d.keys())
         xys = []
         for i in range(0, len(all_keys), 3):
             x, y, _ = pm.enu.geodetic2enu(self.d[all_keys[i]], self.d[all_keys[i+1]], self.d[all_keys[i+2]], self.lat0, self.lon0, self.h0)
-            xys.extend([x, y])
+            xys.extend([x.item(), y.item()])
         
         rospy.set_param("gt_locs", xys)
 
 
 if __name__ == "__main__":
     rospy.init_node("gps_to_local", anonymous=True)
+    while not rospy.has_param("local_pos_ref"): continue
     g2l = GPS2LocalENU()
-    g2l.gps2local()
-
-    # TODO: Check if param is still on server after shutting down node
+    #Check if param is still on server after shutting down node #UPDATE: It is still on server
     rospy.signal_shutdown("Set ground truth fire locations")
