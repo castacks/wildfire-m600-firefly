17da10ce17d25d4b977cf570e59c3a9f9fd1c0ff
Kevin Gmelin <kgmelin@andrew.cmu.edu>
2022-04-19 21:26:36 -0400

    Modified gps_to_local to also publish an occupancy map showing the ground truth locations of the hotspots.

--- firefly_mapping/src/gps_to_local.py
+++ firefly_mapping/src/gps_to_local.py
@@ -4,30 +4,54 @@ import rospy
 import pymap3d as pm
 import yaml
 import rospkg
-
-class GPS2LocalENU:
-    def __init__(self):
-        rospack = rospkg.RosPack()
-        with open(rospack.get_path("firefly_mapping") + "/config/gps_hotspots.yaml", 'r') as f:
-            self.d = yaml.load(f, Loader=yaml.FullLoader)
-        
-        self.lat0, self.lon0, self.h0 = rospy.get_param("local_pos_ref")
-
-        self.gps2local()
-
-    def gps2local(self):
-        all_keys = list(self.d.keys())
-        xys = []
-        for i in range(0, len(all_keys), 3):
-            x, y, _ = pm.enu.geodetic2enu(self.d[all_keys[i]], self.d[all_keys[i+1]], self.d[all_keys[i+2]], self.lat0, self.lon0, self.h0)
-            xys.extend([x.item(), y.item()])
-        
-        rospy.set_param("gt_locs", xys)
-
+from nav_msgs.msg import OccupancyGrid
 
 if __name__ == "__main__":
     rospy.init_node("gps_to_local", anonymous=True)
-    while not rospy.has_param("local_pos_ref"): continue
-    g2l = GPS2LocalENU()
-    #Check if param is still on server after shutting down node #UPDATE: It is still on server
+    pub = rospy.Publisher("gt_map", OccupancyGrid, queue_size=100)
+    rospy.sleep(1)
+
+    rospack = rospkg.RosPack()
+    with open(rospack.get_path("firefly_mapping") + "/config/gps_hotspots.yaml", 'r') as f:
+        d = yaml.load(f, Loader=yaml.FullLoader)
+
+    while not rospy.has_param("local_pos_ref"):
+        continue
+    lat0, lon0, h0 = rospy.get_param("local_pos_ref")
+
+    all_keys = list(d.keys())
+    xys = []
+    for i in range(0, len(all_keys), 3):
+        x, y, _ = pm.enu.geodetic2enu(d[all_keys[i]], d[all_keys[i + 1]], d[all_keys[i + 2]], lat0, lon0, h0)
+        xys.extend([x.item(), y.item()])
+
+    rospy.set_param("gt_locs", xys)
+
+    resolution = 0.5
+    minX = -100
+    maxX = 100
+    minY = -100
+    maxY = 100
+
+    output = OccupancyGrid()
+    output.header.frame_id = "world"
+    output.info.resolution = 0.5
+    output.info.width = 400
+    output.info.height = 400
+    output.info.origin.position.x = -100
+    output.info.origin.position.y = -100
+    output.data = [0] * (400 * 400)
+
+    for x, y in xys:
+
+        gtRow = int((y-minY)/resolution)
+        gtCol = int((x-minX)/resolution)
+
+        gtBin = gtCol + gtRow * 400
+
+        output.data[gtBin] = 100
+
+    pub.publish(output)
+    rospy.sleep(1)
+
     rospy.signal_shutdown("Set ground truth fire locations")
